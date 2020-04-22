d40082da0e0e6013148bb2d8811b56136f83959f
CHENFE2 <felicity.chen@cargosmart.com>
2020-04-23 01:50:17 +0800

    [Felicity] feat: change get user Github information by token in backend and add readme introduction

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 > A Vue.js project
 
+## Introduction
+The frontend demo of OAuth2.0 with Github.
+<hr>
+Backend link: 
+<link>https://github.com/FelicityAmiao/oauth_backend</link>
+<hr>
+Sometime link with Github looks no so stable, may be make this demo run fail. But anyway, I test many times and success when network condition good.
+
+
 ## Build Setup
 
 ``` bash
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -7,8 +7,6 @@
 </template>
 <script>
 
-import * as axios from 'axios'
-
 export default {
   data () {
     return {
@@ -22,19 +20,10 @@ export default {
   mounted () {
     this.code = this.$route.query.code
     this.$http.post(`/api/oauth/${this.code}`).then((res) => {
-      this.accessToken = res.bodyText
-      axios({
-        method: 'get',
-        url: `https://api.github.com/user`,
-        headers: {
-          accept: 'application/json',
-          Authorization: `token ${this.accessToken}`
-        }
-      }).then(res => {
-        this.githubUserName = res.data.login
-        this.avatar_url = res.data.avatar_url
-        this.html_url = res.data.html_url
-      })
+      let result = res.data.data
+      this.githubUserName = result.login
+      this.avatar_url = result.avatar_url
+      this.html_url = result.html_url
     })
   }
 }
